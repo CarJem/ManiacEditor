bb41a6f92b1d6406cad7178d3d57c1d32bfba555
CarJem Generations <Cwallacecs@Gmail.com>
2020-02-01 13:28:57 -0500

    Cleaned Up the Visual Studio Configuration and Other Small Changes

--- ManiacEditor/Properties/AssemblyInfo.cs
+++ ManiacEditor/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("20.01.31.10")]
-[assembly: AssemblyVersion("20.01.31.10")]
-[assembly: AssemblyFileVersion("20.01.31.10")]
+// [assembly: AssemblyVersion("20.01.31.12")]
+[assembly: AssemblyVersion("20.01.31.12")]
+[assembly: AssemblyFileVersion("20.01.31.12")]
