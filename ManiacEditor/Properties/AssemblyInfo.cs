b6eeeceee9fd0ab0bc91d7be3e92c68d3fa7b96e
CarJem Generations <Cwallacecs@Gmail.com>
2020-10-29 15:50:02 -0400

    Build Fixes (TRUE)

--- ManiacEditor/Properties/AssemblyInfo.cs
+++ ManiacEditor/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("20.10.29.42")]
-[assembly: AssemblyFileVersion("20.10.29.42")]
+[assembly: AssemblyVersion("20.10.29.43")]
+[assembly: AssemblyFileVersion("20.10.29.43")]
